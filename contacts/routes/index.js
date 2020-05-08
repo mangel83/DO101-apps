eb7c32aa74b773ec8e968924ab086c12bb123d57
mangel83 <mangel83@gmail.com>
2020-05-08 14:06:04 -0400

    se agrega hostname

--- contacts/routes/index.js
+++ contacts/routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const { pgconn } = require('../db/config')
 const VERSION_APP = process.env.VERSION || 'version no encontrada';
 console.log(VERSION_APP)
+var ip = require("ip");
+var os = require("os");
+var host = os.hostname();
 
 /* Show home page. */
 router.get('/', function(req, res) {
@@ -12,12 +15,12 @@ router.get('/', function(req, res) {
   pgconn.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'contacts')", function(err,results) {
     if (err) {
       console.log(err);
-      res.render('index', { error: 'Database connection failure! '+err.stack, contacts: null, title: 'Contact List', version: VERSION_APP, hostname: req.hostname, ip: req.ip });
+      res.render('index', { error: 'Database connection failure! '+err.stack, contacts: null, title: 'Contact List', version: VERSION_APP, hostname: host, ip: ip.address() });
     }
 
     // 'contacts' table does not exist. Show an empty table.
     else if(results.rows[0].exists == false) {
-      res.render('index', { error: null, contacts: null, title: 'Lista de contactos', version: VERSION_APP, hostname: req.hostname, ip: req.ip});
+      res.render('index', { error: null, contacts: null, title: 'Lista de contactos', version: VERSION_APP, hostname: host, ip: ip.address()});
     }
 
     // 'contacts' table exists. Show the records.
@@ -25,12 +28,12 @@ router.get('/', function(req, res) {
       pgconn.query('SELECT * FROM contacts', function(err,results) {
         if (err) {
           console.log(err);
-          res.render('index', { error: 'Database connection failure! '+err.stack, contacts: null, title: 'Contact List', version: VERSION_APP, hostname: req.hostname, ip: req.ip });
+          res.render('index', { error: 'Database connection failure! '+err.stack, contacts: null, title: 'Contact List', version: VERSION_APP, hostname: req.hostname, ip: ip.address() });
         }
         else {
           let contacts = results.rows;
           console.log(contacts);
-          res.render('index', { error: null, contacts: contacts, title: 'Lista de contactos' , version: VERSION_APP, hostname: req.hostname, ip: req.ip});
+          res.render('index', { error: null, contacts: contacts, title: 'Lista de contactos' , version: VERSION_APP, hostname: host, ip: ip.address()});
         }
       })  
     }
@@ -45,7 +48,7 @@ router.post('/seed', function(req,res) {
   pgconn.query("drop table if exists contacts; create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null); insert into contacts(firstname, lastname, email) values ('Bilbo','Baggins','bilbo@theshire.com'),('Frodo','Baggins','frodo@theshire.com'),('Samwise','Gamgee','sam@theshire.com'),('Peregrin','Took','pippin@theshire.com'),('Meriadoc','Brandybuck','merry@theshire.com')",function(err,results) {
     if (err) {
       console.log(err);
-      res.render('index', { error: 'Seeding database failure! '+err.stack, contacts: null, title: 'Contact List', version: VERSION_AP, hostname: req.hostname, ip: req.ip});
+      res.render('index', { error: 'Seeding database failure! '+err.stack, contacts: null, title: 'Contact List', version: VERSION_AP, hostname: host, ip: ip.address()});
     }
 
     // redirect to the index page
